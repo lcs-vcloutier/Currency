66e015fb6a982eb5a376ac65fbcfa009ed2358d0
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-02 11:23:26 -0500

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Cloutier, Vincent on 2020-11-02.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
