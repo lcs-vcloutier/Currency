0d1cb71b54d3191d484842ca26293bff9a821c6d
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-02 11:38:23 -0500

    started fetch data

--- Currency/ViewModel/FetchData.swift
+++ Currency/ViewModel/FetchData.swift
@@ -9,5 +9,22 @@ import SwiftUI
 
 class FetchData: ObservableObject {
    
+    @Published var conversionData : [Currency] = []
+    
+    func fetch() {
+        let url = "https://api.exchangeratesapi.io/latest?base=USD"
+        
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: URL(string: url)!) { (data, _, _) in
+            
+            guard let JSONData = data else{return}
+            do{
+                
+            }
+            catch{
+                print(error)
+            }
+        }
+    }
 }
 
